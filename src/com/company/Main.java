448d74b7d922e7b87fb94c36cc8b61348d2e0b2b
@sergicarpio <sergi.carpio.ortega@gmail.com>
2021-07-05 11:22:03 +0200

    Primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a;
+        int b;
+        int resultat;
+        a = 1;
+        b = 2;
+        resultat = a + b;
+        System.out.println(resultat);
+    }
+}
